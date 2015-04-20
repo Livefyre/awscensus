bc8f6d9251c793a9fd6c346a4048c7be7c9eb8e3
Nicholas Fowler <nbfowler@gmail.com>
2015-04-20 01:15:31 -0700

    no more marionette, python pdb_query instead

--- reap/__init__.py
+++ reap/__init__.py
@@ -15,10 +15,22 @@ import sys
 import os
 
 from docopt import docopt
+import requests
+import json
 
 import ec2.instances
 import ec2.config
 
+def pdb_query(base_path, q_string = {'query':''}):
+    headers = {"Accept": "application/json"}
+    host = "http://puppetdb:8080"
+    r = requests.get(host+base_path, params=q_string, headers=headers)
+    retval = r.json()
+    return retval
+
+def get_host_list():
+    node_list = [node['certname'] for node in pdb_query("/v2/resources/Class/Lf_base")]
+    return node_list
 
 def main():
     args = docopt(usage)
@@ -31,8 +43,8 @@ def main():
     instances = ec2.instances.get(envs, refresh=True)
     exists = set([x['host'] for x in instances])
 
-    hosts = subprocess.check_output('marionette show_hosts', shell=True)
-    hosts = [x.strip() for x in hosts.strip().split()]
+    hosts = get_host_list()
+    hosts = [x.strip() for x in hosts]
 
     for host in hosts:
         if host not in exists:
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.3',
+    version='0.5',
     description='Tools to gather usage information in AWS',
     # The project's main homepage.
     url='https://github.com/Livefyre/awscensus',
@@ -37,7 +37,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['py-yacc','boto','docopt','simplejson','demjson'],
+    install_requires=['py-yacc','boto','docopt', 'requests', 'simplejson','demjson'],
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
